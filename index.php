f4f2f34ac3496f0bc4e25de6ca9162e301abc276
Aleksander Sebastiański <4868699@teb-gdansk.ad>
2024-04-27 13:01:04 +0200

    adb debug funcon

--- index.php
+++ index.php
@@ -2,14 +2,24 @@
 declare(strict_types=1);
 
 namespace App;
-require_once('./src/view.php');
+
+require_once('./src/View.php');
 include_once('./src/utils/debug.php');
 
-//$_GET
-//$_POST
+const DEFAULT_ACTION ='list';
 
-$action=$_GET['action'] ?? null;
 
+$action=$_GET['action'] ?? DEFAULT_ACTION;
+
+$viewParams = [];
+
+if ($action === 'create'){
+	$page = 'create;'
+	$viewParams ['resultCreate'] ='Udało się dodac notatkę!';
+} else {
+	$page = 'list;'
+	$viewParams ['resultList'] ='Wyświetlamy nową notatkę!';
+}
 
 $view = new view ();
-$view -> render($action);
+$view ->render($page,$viewParams);
--- pages/list.php
+++
@@ -1,1 +0,0 @@
-<h3>lista notatek</h3>
