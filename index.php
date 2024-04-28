d0c6a25128831c652d5811385e420ee0065960ae
Aleksander Sebastiański <4868699@teb-gdansk.ad>
2024-04-28 10:28:13 +0200

    adb debug funcon

--- index.php
+++ index.php
@@ -1,49 +1,11 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App;
 
-require_once('./src/View.php');
+require_once('./src/controller.php');
 include_once('./src/utils/debug.php');
 
-const DEFAULT_ACTION ='list';
-
-
-$action=$_GET['action'] ?? DEFAULT_ACTION;
-	
-$viewParams = [];
-
-// if ($action === 'create'){
-// 	$page = 'create';
-// 	$created = false;
-// 	if (!empty($_POST)) {
-// 		$viewParams=[
-// 			'title'=>$_POST['title'],
-// 			'description'=>$_POST['description'],
-// 		];
-// 		$created = true;
-// 	}
-// 	$viewParams['created'] = $created;
-// } else {
-// 	$page = 'list';
-// 	$viewParams ['resultList'] ='Wyświetlamy nową notatkę!';
-// }
-switch($action){
-	case'create':
-		$page = 'create';
-	 	$created = false;
-		if (!empty($_POST)) {
-			$viewParams=[
-				'title'=>$_POST['title'],
-		 		'description'=>$_POST['description'],
-			];
-			$created = true;
-		}
-		$viewParams['created'] = $created;
-		break;
-	default:
-		$page = 'list';
-		$viewParams ['resultList'] ='Wyświetlamy liste notatek';
-}
-$view = new view ();
-$view ->render($page,$viewParams);
+$controller=new controller($_GET,$_POST);
+$controller->run();
