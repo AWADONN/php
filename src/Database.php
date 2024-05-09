f427fd596ced8a824e57b40f06f1f3a4184d45d6
AWADON <166903021+AWADONN@users.noreply.github.com>
2024-05-09 18:27:18 +0200

    Update Database.php

--- src/Database.php
+++ src/Database.php
@@ -4,14 +4,33 @@ declare (strict_types=1);
 
 namespace App;
 
+use App\Exception\ConfiguratonException;
+use App\Exception\StorageException;
+use Exception;
 use PDO;
+use PDOException;
 
 class Database
 {
     public function __construct(array $config)
     {
-        $dsn ="mysql:dbname={$config['database']};host={$config['host']}";
-
-        $connection = new PDO('lalalalala');
+        try{
+            $this->validateConfig($config);
+            $dsn ="mysql:dbname={$config['database']};host={$config['host']}";
+            $connection = new PDO(
+                $dsn,
+                $config['user'],
+                $config['password'],
+            );
+        } catch (PDOException $e) {
+            throw new storageException("problem z płączeniem do bazy!");
+        }
     }
+    private function ValidateConfig(array $config): void 
+    {
+        if (empty($config['database'])||empty($config['user'])||empty($config['host'])){
+            throw new ConfigurationException("problem z konfiguracją bazy - skontaktuj się z administratorem.");
+        }
+    }
+
 }
