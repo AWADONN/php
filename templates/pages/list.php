4e171644990efec29dd13840b191ac62dc49a5c1
Aleksander Sebastiański <4868699@teb-gdansk.ad>
2024-05-11 11:06:15 +0200

    adb debug funcon

--- templates/pages/list.php
+++ templates/pages/list.php
@@ -1,2 +1,20 @@
-<b><?php echo $params['resultlist']??""?></b>
-<h3>lista notatek</h3>
+<div>
+    <div>
+        <?php
+        if (!empty($prams['before'])){
+            switch($params['before']){
+                case'created':
+                    echo"Notatka została utworzona!";
+                    break;
+                default:
+                    echo "Błędy adres url!";\
+                    break;
+            } 
+        }
+        ?>
+    </div>
+    <b><?php echo $params['resultlist']??""?></b>
+
+    <h3>lista notatek</h3>
+    
+</div>
