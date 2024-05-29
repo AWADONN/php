4e8e315845a2e1923517f0c30bfe7f792dfa7986
AWADON <166903021+AWADONN@users.noreply.github.com>
2024-05-29 12:57:41 +0200

    Update delete.php

--- templates/pages/delete.php
+++ templates/pages/delete.php
@@ -1,12 +1,25 @@
-<h3>usun</h3>
-<div>
-    <?php
-    $note = $params['note']?>
-    <?php if (!empty($unlink(['note']))):?>
-        
-  
-    <?php else :?>
-        
+<div class="delete">
+    <?php $note = $params['note']?? null;?>
+    <?php if ($note):?>
+        <ul>
+            <li> Id: <?php echo (int) $note['id']?></li>
+            <li> Tytuł: <?php echo $note['title']?></li>
+            <li> Opis: <?php echo $note['description']?></li>
+            <li> Utworzono: <?php echo $note['created']?></li>
+            <li>
+                <a href="/">
+                    <button>Powrót do listy notatek</button>
+                </a>
+            </li>
+            <li>
+                <from action="/?action=delete&i=<?php echo (int) $note['id']?>"method="post">
+                    <input type ="text" name="id" value="<?php echo (int) $note['id']?>"hidden>
+                    <input type="submit" value="Usuń">
+                </from>
+
+            </li>
+        </ul>
+    <?php else:?>
+        <div>Brak notatki do wyświetlenia</div>
     <?php endif;?>
 </div>
-    
